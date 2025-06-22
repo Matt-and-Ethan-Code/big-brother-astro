11e39b07b9235e77f50cd49e549a1c6ed1fd0694
gaidukev <90803020+gaidukev@users.noreply.github.com>
2025-06-21 20:01:06 -0600

    added to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://Matt-and-Ethan-code.github.io',
+    base: '/big-brother-astro'
+
+});
