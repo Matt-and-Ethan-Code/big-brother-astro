4ef42ab1613e1ba03ac5135f0b36462b2b20148e
gaidukev <90803020+gaidukev@users.noreply.github.com>
2025-06-25 20:18:01 -0600

    created create_new_session endpoint

---
+++ src/pages/create_new_session.json.ts
@@ -0,0 +1,62 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+await client.connect();
+
+// a function for encoding an id to a string
+const ALPHABET = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789'; 
+const BASE = ALPHABET.length;
+
+const UPPERBOUND = 'KTFXW'
+const UPPERBOUND_ID = 10000052
+
+function encode(num: number) {
+    let str = '';
+    while (num > 0) {
+        const rem = num % BASE;
+        str = ALPHABET[rem] + str;
+        num = Math.floor(num/BASE);
+    }
+    return str || ALPHABET[0];
+}
+
+function decode(str: string) {
+    let num = 0;
+    for (let char of str) {
+        num = num * BASE + ALPHABET.indexOf(char);
+    }
+    return num;
+}
+
+export async function POST() {
+    // get how many games are happening currently -- we don't want too many
+    const roomNameResponse: string | null = await client.get('next_game_id');
+    if (roomNameResponse == null) {
+        return new Response("{}", {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' }
+        })
+    } 
+    const roomName = roomNameResponse;
+
+    const roomID: number = decode(roomName)
+    if (roomID > UPPERBOUND_ID) {
+        return new Response("{}", {
+            status: 403,
+            headers: { 'Content-Type': 'application/json' }
+        })
+    } else {
+        // generate and store next ID
+        const nextID = roomID + 1;
+        const nextGameName = encode(nextID);
+
+        await client.set("next_game_id", nextGameName);
+
+        return new Response(JSON.stringify({
+            roomID: roomName
+        }), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' }
+        })
+    }
+}
