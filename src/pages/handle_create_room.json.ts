e7d71136d3bde3f76ad3217b178a1f8adfc6d608
enortho <ethan@opgenorth.net>
2025-09-10 23:17:55 -0600

    make endpoint a bittt

--- src/pages/handle_create_room.json.ts
+++ src/pages/handle_create_room.json.ts
@@ -1,8 +1,28 @@
 import { createClient } from 'redis';
 
-const client = createClient();
-await client.connect();
+export const prerender = false;
 
-export async function POST() {
+const path_name = `rediss://default:${import.meta.env.UPSTASH_TOKEN}@${import.meta.env.UPSTASH_ENDPOINT}:${import.meta.env.UPSTASH_PORT}`
+console.log("PATH NAME: ", path_name)
+
+const client = createClient({
+    url: path_name
+}).on("error", console.error)
+try {
+    await client.connect();
+} catch(e) {
+    console.log("ERROR! ", e);
+}
+
+export async function POST({params, request} : { request: Request, params: unknown}) {
+    const body = JSON.parse(await request.text()) as unknown;
+    if (!(typeof body == 'object') || body == null) throw new Error('not object!');
+    if (!('username' in body) || typeof body.username != 'string') throw new Error('no username');
+    if (!('game_id' in body) || typeof body.game_id != 'string') throw new Error('no game id');
+    if (!('rankings' in body) || !Array.isArray(body.rankings)) throw new Error('no rankings');
+
+    const {username, game_id, rankings} = body;
+    // do something with these
     
+    return new Response(null, {status:302, headers: {Location: `/`}})
 }
