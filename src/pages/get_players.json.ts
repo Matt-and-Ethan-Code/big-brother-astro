b1b71471cc44e2154cc42474d3674076db757344
gaidukev <90803020+gaidukev@users.noreply.github.com>
2025-09-12 22:05:04 -0600

    finished styling results and connected it to real data

--- src/pages/get_players.json.ts
+++ src/pages/get_players.json.ts
@@ -1,32 +1,60 @@
 import { createClient } from 'redis';
 
-const client = createClient();
-await client.connect();
+export const prerender = false;
+
+const path_name = `rediss://default:${import.meta.env.UPSTASH_TOKEN}@${import.meta.env.UPSTASH_ENDPOINT}:${import.meta.env.UPSTASH_PORT}`
+
+const client = createClient({
+    url: path_name
+}).on("error", console.error)
+try {
+    await client.connect();
+} catch(e) {
+    console.log("ERROR! ", e);
+}
 
 export async function GET( request: any ) {
-    if (request.headers.get('Content-Type') === 'application/json') {
-        const body = await request.json();
-        const roomID = body.roomID;
+    const url = new URL(request.url);
+    console.log("URL: ", url)
+    const roomID = url.searchParams.get('roomCode');
+    console.log("room code: ", roomID);
 
-        // check whether such room exists and whether user hasn't already joined it
-        const roomMembersResponse: string | null = await client.get(roomID);
-        if (roomMembersResponse == null) {
-            return new Response(JSON.stringify({ 
-                errorMessage: "The room you are trying to look up does not exist."
-            }), {
-                status: 404,
-                headers: { 'Content-Type': "application/json" }
-            })
-        } else {
 
-            const roomMembersData = JSON.parse(roomMembersResponse);
+    if (!roomID) {
+        return new Response(JSON.stringify({ 
+            errorMessage: `Invalid room code. ${url.searchParams}`
+        }), {
+            status: 404,
+            headers: { 'Content-Type': "application/json" }
+        })
+    }
 
-            return new Response(JSON.stringify({
-                players: roomMembersResponse
-            }), {
-                status: 400,
-                headers: { 'Content-Type': 'application/json' }
-            })
-        }
+    // check whether such room exists and whether user hasn't already joined it
+    const roomMembersResponse: string | null = await client.get(roomID);
+    if (roomMembersResponse == null) {
+        return new Response(JSON.stringify({ 
+            errorMessage: "The room you are trying to look up does not exist."
+        }), {
+            status: 404,
+            headers: { 'Content-Type': "application/json" }
+        })
+    } else {
+
+        const allRoomMembersData = JSON.parse(roomMembersResponse);
+
+        // now get all members who submitted
+        // keys of those members will be in sequence USERNAME|roomID
+        const keys = await client.keys(`*|${roomID}`);
+        const usernames = keys.map(key => key.split('|')[0]);
+
+
+
+        return new Response(JSON.stringify({
+            players: roomMembersResponse,
+            completedPlayers: usernames
+        }), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' }
+        })
     }
 }
