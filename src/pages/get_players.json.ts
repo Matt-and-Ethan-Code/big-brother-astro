c736ee962b75de221bc7c21f9b2bfec6ca70253e
gaidukev <90803020+gaidukev@users.noreply.github.com>
2025-09-12 22:23:00 -0600

    fixed data bug with results data fetching

--- src/pages/get_players.json.ts
+++ src/pages/get_players.json.ts
@@ -13,12 +13,31 @@ try {
     console.log("ERROR! ", e);
 }
 
+async function findKeysByPattern(pattern: string) {
+    const keys: string[] = [];
+    let cursor = "0";
+    console.log("pattern: ", pattern);
+
+    do {
+        const reply = await client.scan(cursor, {
+            MATCH: pattern,
+            COUNT: 100
+        });
+
+        console.log("reply: ", reply);
+        const nextCursor = reply.cursor;
+        const newKeys = reply.keys;
+        cursor = nextCursor;
+
+        keys.push(...newKeys);
+    } while (cursor !== "0");
+
+    return keys;
+}
+
 export async function GET( request: any ) {
     const url = new URL(request.url);
-    console.log("URL: ", url)
     const roomID = url.searchParams.get('roomCode');
-    console.log("room code: ", roomID);
-
 
     if (!roomID) {
         return new Response(JSON.stringify({ 
@@ -44,13 +63,13 @@ export async function GET( request: any ) {
 
         // now get all members who submitted
         // keys of those members will be in sequence USERNAME|roomID
-        const keys = await client.keys(`*|${roomID}`);
-        const usernames = keys.map(key => key.split('|')[0]);
+        const keys = await findKeysByPattern(`*:${roomID}`);//await client.keys(`*|${roomID}`);
+        const usernames = keys.map(key => key.split(':')[0]);
 
 
 
         return new Response(JSON.stringify({
-            players: roomMembersResponse,
+            players: allRoomMembersData,
             completedPlayers: usernames
         }), {
             status: 200,
