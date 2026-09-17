bae23b71bfe408a4994a3ba892c2999bffdad25f
enortho <ethan@opgenorth.net>
2026-09-16 20:07:31 -0600

    fix scoring + display

--- src/util/score.ts
+++ src/util/score.ts
@@ -24,70 +24,51 @@ export const REMAINING = [
 ];
 
 function score_impl(predicted: string[], actual: string[], remaining: string[]) {
+  const index_of_last_evicted_player = actual.length - 1;
   let score_per_person: { [name: string]: { distance: number, contribution: number } } = {};
   let total_score = 0;
-  for (let i = 0; i < predicted.length; ++i) {
-    const pred = predicted[i];
-    const predicted_evicted_place = i;
-    const actual_evicted_place = actual.indexOf(pred);
-    const player_still_in = actual_evicted_place == -1;
 
-    let distance: null | number = null;
-    if (player_still_in) {  // they havent been evicted yet (they should be in the remaining list)
-      // they can still get points based on how close they put the prediction to the end
-      const is_player_known = remaining.includes(pred);
-      if (!is_player_known) {
-        console.warn(`UNKNOWN PLAYER: ${pred}`);
-        continue;
-      }
+  function set_score(player: string, distance: number, score_override?: number) {
+    const contribution = score_override == null ? Math.max(0, 3 - distance) : score_override;
+    score_per_person[player] = {
+      distance,
+      contribution,
+    }
+    total_score += contribution;
+  }
 
-      const at_remaining_part = i >= actual.length;
-      if (at_remaining_part && remaining.includes(pred)) {
-        score_per_person[pred] = {
-          distance: 0,
-          contribution: 2
-        };
-        total_score += 2;
-        continue;
-      }
-      else {
-        const remaining_place = actual.length; // treat "still in the game" as a bonus entry in the `actual` list
-        distance = Math.abs(predicted_evicted_place - remaining_place);
-      }
-      
+  // score for players that have been evicted
+  for (let i = 0; i < actual.length; ++i) {
+    const player = actual[i];
+    const predicted_evicted_place = predicted.indexOf(player);
+    if (predicted_evicted_place == -1) {
+      console.warn(`PLAYER NOT FOUND IN USER PREDICTIONS: ${player}`);
+      continue;
     }
 
-    if (distance == null) {
-      distance = Math.abs(predicted_evicted_place - actual_evicted_place);
+    const distance = Math.abs(predicted_evicted_place - i);
+    set_score(player, distance);
+  }
+
+  // score for players that are still in
+  for (let i = 0; i < remaining.length; ++i) {
+    const player = remaining[i];
+    const guessed_position = predicted.indexOf(player);
+    if (guessed_position == -1) {
+      console.warn(`PLAYER NOT FOUND IN USER PREDICTIONS: ${player}`);
+      continue;
     }
-    
-    if (distance == 0) {
-      score_per_person[pred] = {
-        distance,
-        contribution: 3
-      };
-      total_score += 3;
-    }
-    else if (distance == 1) {
-      score_per_person[pred] = {
-        distance,
-        contribution: 2
-      };
-      total_score += 2;
-    }
-    else if (distance == 2) {
-      score_per_person[pred] = {
-        distance,
-        contribution: 1
-      };
-      total_score += 1;
+
+    const correctly_placed_them_at_the_end = guessed_position >= index_of_last_evicted_player;
+    if (correctly_placed_them_at_the_end) {
+      set_score(player, 0, 2);
     }
     else {
-      score_per_person[pred] = {
-        distance,
-        contribution: 0
-      };
-      total_score += 0;
+      // didn't place them at the end but maybe they still put them close to the end
+      // treat the remaining players like one big block (order doesn't matter)
+      const pretend_houseguest_position = index_of_last_evicted_player + 1;
+      const distance = Math.abs(guessed_position - pretend_houseguest_position);
+      set_score(player, distance);
     }
   }
 
@@ -101,3 +82,4 @@ export function score(predicted: string[]) {
   return score_impl(predicted, ACTUAL, REMAINING);
 }
 
+export const MAX_SCORE: number = score_impl([...ACTUAL, ...REMAINING], ACTUAL, REMAINING).total;
